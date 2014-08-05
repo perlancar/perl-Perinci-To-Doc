4419ac72030e5eadd6cf042b7b194decb7361dfb
Steven Haryanto (on Asus Zenbook) <stevenharyanto@gmail.com>
2014-08-05 13:32:38 +0700

    Add Synopsis and function doc

--- lib/Perinci/ToUtil.pm
+++ lib/Perinci/ToUtil.pm
@@ -1,21 +1,19 @@
 package Perinci::ToUtil;
 
+# DATE
+# VERSION
+
 use 5.010;
 use strict;
 use warnings;
 
-# VERSION
-
-# generate human-readable short description of schema, this will be
-# handled in the future by Sah itself (using the human compiler).
-
 sub sah2human_short {
-    require Data::Sah;
+    require Data::Sah::Normalize;
     require Function::Fallback::CoreOrPP;
 
     my ($s) = @_;
     if ($s->[0] eq 'any') {
-        my @alts    = map {Data::Sah::normalize_schema($_)}
+        my @alts    = map {Data::Sah::Normalize::normalize_schema($_)}
             @{$s->[1]{of} // []};
         my @types   = map {$_->[0]} @alts;
         @types      = sort(Function::Fallback::CoreOrPP::uniq(@types));
@@ -28,8 +26,22 @@ sub sah2human_short {
 1;
 # ABSTRACT: Temporary utility module
 
+=head1 SYNOPSIS
+
+ use Perinci::ToUtil qw(sah2human_short);
+
+ say sah2human_short("int"); # -> int
+ say sah2human_short(["int", {req=>1, min=>0}]); # -> int
+ say sah2human_short(["any", of=>["int*", [array=>of=>'int*']]]); # -> int|array
+
+
 =head1 FUNCTIONS
 
-=head2 sah2human_short
+=head2 sah2human_short($sch) -> str
+
+Generate human-readable short description of schema, basically just type name
+(e.g. C<int>) or a list of type names (e.g. C<code|hash>). This is a temporary
+function and will be handled in the future by L<Data::Sah> using the human
+compiler.
 
 =cut
